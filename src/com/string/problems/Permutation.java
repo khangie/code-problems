ca1fa0776d5ef04e03f37976e6d6323bf063741f
Khang Nguyen <khangie@hotmail.com>
2019-01-11 14:13:42 -0800

    permutation

--- src/com/string/problems/Permutation.java
+++ src/com/string/problems/Permutation.java
@@ -13,8 +13,16 @@ public class Permutation {
 		String input2 = "tste";
 		
 		System.out.println(isPermutation(input1, input2));
+		System.out.println(isPermutation2(input1, input2));
 		
 	}
+	
+	/**
+	 * Two strings are permutations if the sorted results are equal
+	 * @param input1
+	 * @param input2
+	 * @return
+	 */
 	
 	public static boolean isPermutation(String input1, String input2) {
 		
@@ -32,6 +40,37 @@ public class Permutation {
 		java.util.Arrays.sort(charArray);
 		return new String(charArray);
 		
+	}
+	
+	/**
+	 * Two strings are permutations if they have equal character counts
+	 * @param input1
+	 * @param input2
+	 * @return
+	 */
+	
+	public static boolean isPermutation2(String input1, String input2) {
+		
+		if (input1.length() != input2.length()) {
+			return false;
+		}
+		
+		int[] letters = new int[128];
+		
+		for (int i = 0; i < input1.length(); i++) {
+			int charVal = input1.charAt(i);
+			letters[charVal]++;
+		}
+		
+		for (int i = 0; i < input2.length(); i++) {
+			int charVal = input2.charAt(i);
+			letters[charVal]--;
+			if (letters[charVal] < 0) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 	
 }
