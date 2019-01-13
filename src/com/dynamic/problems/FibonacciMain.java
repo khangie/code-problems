833eaeb55917c49f9ff11b456888421a5dac1ece
Khang Nguyen <khangie@hotmail.com>
2019-01-13 09:11:01 -0800

    fib iterative updates

--- src/com/dynamic/problems/FibonacciMain.java
+++ src/com/dynamic/problems/FibonacciMain.java
@@ -72,17 +72,16 @@ public class FibonacciMain {
 	
 	public static int fibDynamicIterative(int n) {
 		
-		int[] f = new int[n];
-		int i;
+		int[] f = new int[n + 1];
 		
-		f[0] = 1;
+		f[0] = 0;
 		f[1] = 1;
 		
-		for (i = 2; i < n; i++) {
-			f[i] = f[i -1] + f[i - 2];
+		for (int i = 2; i <= n; i++) {
+			f[i] = f[i - 1] + f[i - 2];
 		}
 		
-		return f[n - 1];
+		return f[n];
 		
 	}
 	
